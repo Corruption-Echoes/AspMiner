76cf240b3b323c91ab57675b62aa4bfba9bc3ced
Aric <morr0457@algonquinlive.com>
2023-07-19 16:46:53 -0400

    Proof of concept
    Tiny idle clicker game using webforms and javascript

---
+++ mining.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="mining.aspx.cs" Inherits="idleMiner.mining" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="oreDisplay" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="minerDisplay" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="debugDisplay" runat="server" Text=""></asp:Label>
+            <asp:Button ID="Mine" runat="server" Text="Dig" Onclick="ManualMine"/>
+            <asp:Button ID="JavaMine" hidden="hidden" runat="server" Text="Dig" OnClick="AutoMine"/>
+            <asp:Button ID="Purchase" runat="server" Text="Buy Miner" OnClick="PurchaseMiner"/>
+            <br />
+            <progress id="automineTimer" value="0" max="100" runat="server"></progress>
+            <input id="timerPort" runat="server" hidden="hidden"></input>
+        </div>
+        <script>
+            const delay = 1000
+            const chunkCount=100
+            //Enable auto mining
+             var intervalID = setInterval(updatetimer, delay / chunkCount)
+            
+            function updatetimer() {
+                var progress = document.getElementById("automineTimer")
+                var port=document.getElementById("timerPort")
+                progress.value = 100 / chunkCount + progress.value
+                port.value = progress.value
+                console.log("updateTimer triggered")
+                if (progress.value >= 100) {
+                    document.getElementById("JavaMine").click()
+                    clearInterval(intervalID)
+                }
+            }
+        </script>
+    </form>
+</body>
+</html>
---
+++ mining.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace idleMiner
+{
+    public partial class mining : System.Web.UI.Page
+    {
+        string ore = "ore";
+        string miners = "miners";
+        string timer = "timerProgress";
+        string exponent = "divider";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //enableAutoPost();//Don't need this with the .click methodology
+            if (!IsPostBack)
+            {
+                Session[ore] = 0;
+                Session[miners] = 0;
+                Session[timer] = 0;
+                Session[exponent] = 16f;
+            }
+            updateUI();
+        }
+        public void mineOre(int bonusOre)
+        {
+            Session[ore] = getSessionData(ore)+bonusOre;
+            updateUI();
+        }
+        public void storeTimerProgress()
+        {
+            Session[timer] = int.Parse(timerPort.Value);
+        }
+        public void updateUI()
+        {
+            automineTimer.Attributes["value"] = getSessionData(timer).ToString();
+            oreDisplay.Text = "Ore :" + getSessionData(ore);
+            minerDisplay.Text = "Miners :" + getSessionData(miners);
+            Purchase.Text = "Buy Miner : " + calculateMinerPrice() + "G";
+        }
+        public void PurchaseMiner(Object sender, EventArgs e)
+        {
+            if (getSessionData(ore) >= calculateMinerPrice())
+            {
+                Session[ore] = getSessionData(ore) - calculateMinerPrice();
+                Session[miners] = getSessionData(miners) + 1;
+            }
+            storeTimerProgress();
+            updateUI();
+        }
+        public void ManualMine(Object sender, EventArgs e)
+        {
+            storeTimerProgress();
+            mineOre(1);
+            updateUI();
+        }
+        public void AutoMine(Object sender, EventArgs e)
+        {
+            Session[ore] = getSessionData(ore) + getSessionData(miners);
+            Session[timer] = 0;
+            updateUI();
+        }
+        public int getSessionData(string field)
+        {
+            if (Session[field]!= null)
+            {
+                return (int)Session[field];
+            }
+            return 0;
+        }
+        public int calculateMinerPrice()
+        {
+            int toReturn = (int)Math.Pow(10, 1+(float)getSessionData(miners)/getSessionData(exponent));
+            if(toReturn < 10)
+            {
+                toReturn = 10;
+            }
+            return toReturn;
+        }
+        /*public void enableAutoPost()//Deprecated method for allowing javascript to do postbacks
+        {// .click method is cleaner
+            ClientScript.GetPostBackEventReference(JavaMine, "");
+        }*/
+    }
+}
